bdd69682e6435d29e373f6fa0526458434234ea0
David Dubinin <david_dubinin@disroot.org>
2025-10-28 20:03:41 +0100

    testing transfer

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <b15f/b15f.h>
 
-int main(void){
+B15F& setup(){
     B15F& drv = B15F::getInstance();
-    
-    drv.setRegister(&DDRA, 0x1);
+    drv.setRegister(&DDRA, 0b00001111);
+    return drv;
+}
+
+uint8_t readData(B15F& drv){ return (drv.getRegister(&PORTA) >> 4); }
+
+void writeData(B15F& drv, volatile uint8_t value){
+    drv.setRegister(&PORTA, value);
+}
+
+
+int main(void){
+    B15F& drv = setup();
 
     while(1){
-        drv.setRegister(&PORTA, drv.getRegister(&PORTA) ^ 0x1);
-        drv.delay_ms(1000);
+        writeData(drv, drv.getRegister(&PORTA) ^ 0b1111);;
+
+        uint8_t data = readData(drv);
+
+        if(data){
+            std::cout << data << std::endl;
+        }
+
+        drv.delay_ms(500);
     }
-   
+
     return 0;
 }
